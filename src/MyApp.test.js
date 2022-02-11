2d65813b4a2a75264a9b56b55988f9e33a1066c9
BJ Klingenberg <bklingen@calpoly.edu>
2022-02-11 11:05:33 -0800

    udpate test

---
+++ src/MyApp.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+q;
+import App from "./MyApp";
+
+test("renders page with Submit button", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Submit/i);
+  expect(linkElement).toBeInTheDocument();
+});
