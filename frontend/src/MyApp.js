3a8d3fbc15c55208995635088031d30daad383f5
BJ Klingenberg <bklingen@calpoly.edu>
2023-03-01 15:38:23 -0800

    back to backend url

--- frontend/src/MyApp.js
+++ frontend/src/MyApp.js
@@ -8,8 +8,8 @@ import axios from "axios";
 function MyApp() {
   // const API_BASE_URL = 'http://localhost:5000/users';
   // const API_BASE_URL = "https://csc307-api-bklingen.herokuapp.com/users";
-  // const API_BASE_URL = "https://csc307-backend.azurewebsites.net/";
-  const API_BASE_URL = "https://azure-csc307-api.azurewebsites.net/";
+  const API_BASE_URL = "https://csc307-backend.azurewebsites.net/";
+  // const API_BASE_URL = "https://azure-csc307-api.azurewebsites.net/";
 
   const [characters, setCharacters] = useState([]);
 
